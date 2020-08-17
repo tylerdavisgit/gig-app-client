e128bc0a1691dd6f5f42755117d3afef3d13abeb
Tyler Davis <tylerdavis@Tylers-iMac-2.local>
2020-08-16 21:16:02 -0400

    Update API Urls with apiConfig.

---
+++ gig-app-client/build/precache-manifest.323ca6537daceb27c4645f2ffedc47dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75f0b55272e945de18f18f874578f467",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab1c0fb4ffb066f0779e",
+    "url": "/static/css/main.6edf3726.chunk.css"
+  },
+  {
+    "revision": "c40dbf9d882a4ec2eb23",
+    "url": "/static/js/2.d966f763.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d966f763.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab1c0fb4ffb066f0779e",
+    "url": "/static/js/main.cad3d7b1.chunk.js"
+  },
+  {
+    "revision": "458d73851fee059f0f9a",
+    "url": "/static/js/runtime-main.bfff3ab1.js"
+  }
+]);
