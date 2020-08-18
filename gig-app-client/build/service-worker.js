4c4ea46b3901707f4ac76def3469202614294dca
Tyler Davis <tylerdavis@Tylers-iMac-2.local>
2020-08-18 10:12:51 -0400

    Added links to gigs in calendar view that allows them to be clicked and edited.

--- gig-app-client/build/service-worker.js
+++ gig-app-client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3a0d27d969dc4143d27e068a8b9e11d4.js"
+  "/precache-manifest.045fba33e7261579a98a0a4de4fadb88.js"
 );
 
 self.addEventListener('message', (event) => {
